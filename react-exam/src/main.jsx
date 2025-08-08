e02ed4d2730efc37831fba1d449483a7a5c34fb9
cochae <ycw1203@naver.com>
2025-08-08 11:17:43 +0900

    0808 commit 2

--- react-exam/src/main.jsx
+++ react-exam/src/main.jsx
@@ -1,8 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { legacy_createStore as createStore } from "redux";
+import reducers from "./ex070/redux/reducers";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const store = createStore(reducers);
+const root = createRoot(document.getElementById("root"));
+
+const listner = () => {
+  root.render(<App store={store} />);
+};
+
+store.subscribe(listner);
+listner();
